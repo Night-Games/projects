dae4c678646ac1e8ca4fc2f58b281e9d1179fa6e
Night <Meus_Jogos_Zerados_e_Favoritos@outlook.com>
2024-09-06 08:22:40 -0300

    Update taskpane.js

--- taskpane.js
+++ taskpane.js
@@ -1,9 +1,8 @@
 Office.onReady(info => {
     if (info.host === Office.HostType.Excel) {
-        Office.context.ui.addHandlerAsync(Office.EventType.DialogMessageReceived, function (event) {
-            if (event.message === 'addVideo') {
-                addContentAppToSheet();
-            }
+        // Verifique se o Office está pronto
+        $("#addVideoButton").on("click", function() {
+            addContentAppToSheet();
         });
     }
 });
@@ -12,7 +11,7 @@ async function addContentAppToSheet() {
     try {
         await Excel.run(async (context) => {
             const sheet = context.workbook.worksheets.getActiveWorksheet();
-            const range = sheet.getRange("A1"); // Você pode definir onde deseja adicionar o conteúdo
+            const range = sheet.getRange("A1"); // Ajuste conforme necessário
             range.values = [["<iframe width='560' height='315' src='https://www.youtube.com/embed/CjP3VlbIfDA' frameborder='0' allowfullscreen></iframe>"]];
             await context.sync();
         });
