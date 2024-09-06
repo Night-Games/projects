a590fe6b46b3528d86c82d32b610ab27936536df
Night <Meus_Jogos_Zerados_e_Favoritos@outlook.com>
2024-09-06 08:38:08 -0300

    Update taskpane.js

--- taskpane.js
+++ taskpane.js
@@ -1,21 +1,8 @@
-Office.onReady(info => {
-    if (info.host === Office.HostType.Excel) {
-        // Verifique se o Office está pronto
-        $("#addVideoButton").on("click", function() {
-            addContentAppToSheet();
-        });
-    }
-});
-
-async function addContentAppToSheet() {
-    try {
-        await Excel.run(async (context) => {
-            const sheet = context.workbook.worksheets.getActiveWorksheet();
-            const range = sheet.getRange("A1"); // Ajuste conforme necessário
-            range.values = [["<iframe width='560' height='315' src='https://www.youtube.com/embed/CjP3VlbIfDA' frameborder='0' allowfullscreen></iframe>"]];
-            await context.sync();
-        });
-    } catch (error) {
-        console.error('Erro ao adicionar conteúdo: ', error);
-    }
+// script.js
+// Função para atualizar o vídeo com um URL específico
+function setVideoUrl(url) {
+    document.getElementById('videoIframe').src = url;
 }
+
+// Exemplo de uso
+setVideoUrl('https://www.youtube.com/embed/CjP3VlbIfDA');
