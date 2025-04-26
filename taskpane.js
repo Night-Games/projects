d221185d1f2db06c1dd0240977a6a4a5226a9ab2
Night <Meus_Jogos_Zerados_e_Favoritos@outlook.com>
2025-04-26 14:30:01 -0300

    removendo arquivos desnecessários

--- taskpane.js
+++
@@ -1,8 +0,0 @@
-// script.js
-// Função para atualizar o vídeo com um URL específico
-function setVideoUrl(url) {
-    document.getElementById('videoIframe').src = url;
-}
-
-// Exemplo de uso
-setVideoUrl('https://www.youtube.com/embed/CjP3VlbIfDA');
